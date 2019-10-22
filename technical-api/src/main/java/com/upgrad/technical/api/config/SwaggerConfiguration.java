23d9e0453d09ed51fc7ce1b269cf836b5cadd6a6
ArchanaArige <nigam_archana@yahoo.co.in>
2019-10-22 23:59:33 +0530

    second commit

--- technical-api/src/main/java/com/upgrad/technical/api/config/SwaggerConfiguration.java
+++ technical-api/src/main/java/com/upgrad/technical/api/config/SwaggerConfiguration.java
@@ -10,6 +10,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //adding changesgit branch
+//adding one more changegitechnical-api/src/main/java/com/upgrad/technical/api/config/SwaggerConf
+//iguration.java
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
